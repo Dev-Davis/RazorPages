a90f04e0fa66c37ce319cddf131f1151a206406b
Sean Davis <sean.dev.davis@gmail.com>
2020-06-03 01:20:47 -0500

    start of designing book index

--- BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor.Pages.BookList
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDBContext _db;
 
+        public IndexModel(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
         }
     }
 }
