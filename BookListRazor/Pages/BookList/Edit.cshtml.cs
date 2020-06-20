2c8bd0b939679df4b21548bda38f0bc6282657d4
Sean Davis <sean.dev.davis@gmail.com>
2020-06-19 20:33:45 -0500

    added route to edit page

---
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+
+        private ApplicationDBContext _db;
+
+        public EditModel(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+    }
+}
